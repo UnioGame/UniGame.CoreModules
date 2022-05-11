b5913a79a489b0edcec4fdecabee64ad8af8565c
Mefodei <mefodei.link@gmail.com>
2022-05-11 22:31:00 +0300

    - update rx module

--- UniGame.Neuecc.Unirx/UniRx/Scripts/Observer.cs
+++ UniGame.Neuecc.Unirx/UniRx/Scripts/Observer.cs
@@ -414,10 +414,10 @@ namespace UniRx
             return source.Subscribe(Observer.CreateSubscribeObserver(onNext, Stubs.Throw, Stubs.Nop));
         }
 
-        // public static IDisposable RxSubscribe<T>(this IObservable<T> source, Action onNext)
-        // {
-        //     return source.RxSubscribe(x => onNext());
-        // }
+        public static IDisposable RxSubscribe<T>(this IObservable<T> source, Action onNext)
+        {
+            return source.RxSubscribe(x => onNext());
+        }
         
         public static IDisposable RxSubscribe<T>(this IObservable<T> source, Action<T> onNext, Action<Exception> onError)
         {
