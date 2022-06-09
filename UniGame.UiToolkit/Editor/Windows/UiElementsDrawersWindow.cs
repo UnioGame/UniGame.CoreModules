a0e36dd5a0c63e900496c4a3017b72bfcb1abc08
Mefodei <mefodei.link@gmail.com>
2022-06-09 19:49:45 +0300

    move rx module

--- UniGame.UiToolkit/Editor/Windows/UiElementsDrawersWindow.cs
+++ UniGame.UiToolkit/Editor/Windows/UiElementsDrawersWindow.cs
@@ -7,11 +7,9 @@ namespace UniModules.UniGame.UiElements.Editor.Windows
     using System;
     using System.Collections.Generic;
     using UniModules.Editor;
-    using global::UniGame.Core.EditorTools.Editor.UiElements;
     using Sirenix.OdinInspector;
     using TypeDrawers;
     using UniModules.UniCore.Runtime.DataFlow;
-    using UniModules.UniCore.Runtime.Rx.Extensions;
     using UniRx;
     using UnityEditor;
     using UnityEngine;
